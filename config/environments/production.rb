8a26e56c5257af0a8f5bcd30d8e2581f6d3cfb2d
David <davn.st@gmail.com>
2022-10-21 02:49:27 +0200

    back to main

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,14 +2,6 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.action_mailer.default_url_options = {
-    :host => "mail.cosmoswide.fr",
-    :port => 3000,
-    :authentication => :plain,
-    :user_name => Rails.application.credentials.SENDGRID_USERNAME,
-    :password => Rails.application.credentials.SENDGRID_PASSWORD,
-    :enable_starttls_auto => true
-  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
